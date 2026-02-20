56381a185792071230fdcdcffb085c8355e8c6bd
amabyado <amabyado@up.edu.ph>
2026-02-20 23:32:35 +0800

    initial

---
+++ src/icons/Notebook.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from "react";
+
+export default function IconParkSolidNotebook(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}>{/* Icon from IconPark Solid by ByteDance - https://github.com/bytedance/IconPark/blob/master/LICENSE */}<defs><mask id="SVGEqx9mcRF"><g fill="none" strokeLinejoin="round" strokeWidth="4"><path fill="#fff" stroke="#fff" d="M10 6a2 2 0 0 1 2-2h28a2 2 0 0 1 2 2v36a2 2 0 0 1-2 2H12a2 2 0 0 1-2-2z" /><path stroke="#000" strokeLinecap="round" d="M34 6v36" /><path stroke="#fff" strokeLinecap="round" d="M6 14h8M6 24h8M6 34h8M27 4h12M27 44h12" /></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#SVGEqx9mcRF)" /></svg>
+  )
+}
